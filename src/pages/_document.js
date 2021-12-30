8a4aca5e9a8277f97418685ccfe121a165d8b0b3
klinkonskydev <klinkonskydev@gmail.com>
2021-12-30 18:19:31 -0300

    #Feature - Favicon added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -16,6 +16,7 @@ class Doc extends Document {
             href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@100;200;300;400;500;600;700&display=swap"
             rel="stylesheet"
           />
+          <link rel="icon" type="image/x-icon" href="/favicon.svg" />
         </Head>
         <body>
           <Main />
